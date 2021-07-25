ec63ab07251dd14f9e0319cdc6d86c982b9fb322
jztlucky6264 <77235457+jztlucky6264@users.noreply.github.com>
2021-07-24 15:29:13 -1200

    my last commit

--- src/NavSearch.jsx
+++ src/NavSearch.jsx
@@ -10,38 +10,32 @@ const NavSearch = (props) => {
     <>
       <nav className="navbar navbar-horizontal navbar-expand-lg navbar-dark bg-white">
         <div className="container">
-          <a className="Clients">
-            <span>Clients</span>
-          </a>
-          <div className="form-group">
-            <div className="input-group">
-              <input
-                ref={inputE1}
-                value={props.input}
-                type="text"
-                className="form-control w-100   "
-                placeholder="Search by customer name/email"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-                onChange={handleInput}
-              />
+          <div className="row">
+            <div className="col-8 col-md-4">
+              <a className="Clients">
+                <span>Clients</span>
+              </a>
             </div>
-          </div>
+            <div className="form-group col-12 col-md-4">
+              <div className="input-group">
+                <input
+                  ref={inputE1}
+                  value={props.input}
+                  type="text"
+                  className="form-control w-100   "
+                  placeholder="Search by customer name/email"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                  onChange={handleInput}
+                />
+              </div>
+            </div>
 
-          <div>
-            <button
-              onClick={props.onSearch}
-              className="btn btn-primary"
-              type="button"
-            >
-              Search
-            </button>
-          </div>
-
-          <div>
-            <button className="btn btn-primary" type="button">
-              Add New Customer
-            </button>
+            <div className="col-12 col-md-4">
+              <button className="btn btn-primary" type="button">
+                Add New Customer
+              </button>
+            </div>
           </div>
         </div>
       </nav>
