8ac1b03b78932f2c88ab6078c838ccc5fc1d892b
jztlucky6264 <77235457+jztlucky6264@users.noreply.github.com>
2021-07-25 14:21:22 -1200

    my mistake commit

--- src/NavSearch.jsx
+++ src/NavSearch.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 const NavSearch = (props) => {
-  const inputE1 = useRef("");
-
   function handleInput(e) {
     props.Searchkeyword(e.target.value);
   }
@@ -10,16 +8,15 @@ const NavSearch = (props) => {
     <>
       <nav className="navbar navbar-horizontal navbar-expand-lg navbar-dark bg-white">
         <div className="container">
-          <div className="row">
-            <div className="col-8 col-md-4">
+          <div className="row container-fluid">
+            <div className="col-8 col-md-2">
               <a className="Clients">
                 <span>Clients</span>
               </a>
             </div>
-            <div className="form-group col-12 col-md-4">
+            <div className="form-group col-12 col-md-auto">
               <div className="input-group">
                 <input
-                  ref={inputE1}
                   value={props.input}
                   type="text"
                   className="form-control w-100   "
@@ -31,7 +28,7 @@ const NavSearch = (props) => {
               </div>
             </div>
 
-            <div className="col-12 col-md-4">
+            <div className="col-12 col-md-4 col-lg-3  ">
               <button className="btn btn-primary" type="button">
                 Add New Customer
               </button>
